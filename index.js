4c6cd30fca5f4b7d69e773395d648b201b24c436
yoshi-sho-0606 <shoyoshioka23060202@icloud.com>
2022-07-05 11:18:39 +0900

    タスクの総件数、完了済みの件数を表示できるようにした（ステータスを伴ってないので総件数と完了済みの表示は今の所ずれる）

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ console.log('hello world')
 // タスクの配列を作成
 let tasks = ['task1', 'task2', 'task3']
 
+// タスクの総件数
+let tasksLength = tasks.length
+
+// 完了済みのタスクの件数
+let doneTasksLength = 0
+
+// タスクの総件数表示
+totalTask()
+
+// 完了済みタスクの件数
+doneTask(0)
+
 // taskの初期表示
 tasks.forEach((task, index) => {
   additionTask(task, index)
@@ -17,15 +29,13 @@ submitButton.addEventListener('click', function addTask() {
   
   if(newTask != ''){
     console.log(newTask)
-    additionTask(newTask, (tasks.length))
+    additionTask(newTask, tasks.length)
     document.getElementById('addTaskName').value = ''
     tasks.push(newTask)
   }else{
     window.alert('タスク名を入力してください！')
   }
 }, false)
-
-
 
 // table要素にtaskListの中身を表示する処理の本体
 function additionTask(task, index) {
@@ -52,11 +62,13 @@ function changeStatus(event) {
     target.classList.remove('done')
     event.target.innerText = 'done'
     event.target.classList.remove('btn-success')
+    doneTask(-1)
   }else{
     target.innerHTML = `<del class='bg-light'>${target.innerText}</del>`
     target.classList.add('done')
     event.target.innerText = 'start'
     event.target.classList.add('btn-success')
+    doneTask(1)
   }
 }
 
@@ -64,4 +76,19 @@ function deleteTask(event) {
   let targetId = event.target.id.split('-')[1]
   let row = document.getElementById(`row-${targetId}`)
   row.remove()
+  tasksLength -= 1
+  totalTask()
+}
+
+// タスクの件数表示本体
+function totalTask() {
+  let total = document.getElementById('task-total')
+  total.innerText = `total: ${tasksLength}`
+}
+
+// タスクの完了件数表示本体
+function doneTask(int) {
+  doneTasksLength += int
+  let done = document.getElementById('task-done')
+  done.innerText = `done: ${doneTasksLength}`
 }
